ddbcc36fee7456034df51e628b4f5e8081863a85
Benjamin W <benjamin-w@hotmail.com>
2017-01-07 00:49:22 +0800

    update getData
    1. add on paste
    2. check previous input data are the same or not ( avoid keyboard function key trigger $.post, ex. ctrl + c )

--- src/ezAutoSuggestion/ezAutoSuggestion.js
+++ src/ezAutoSuggestion/ezAutoSuggestion.js
@@ -1,3 +1,21 @@
+/*
+The MIT License (MIT)
+
+ * Copyright (c) 2016 Benjamin Wong, benjamin-w@hotmail.com
+ * https://github.com/yben56/ez-auto-suggestion
+ * Dependencies - jquery-3.1.1, Bootstrap v3.3.6
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 (function($){
 	'use strict';
 	
@@ -19,30 +37,33 @@
 	};
 	
 	init.prototype.getData = function(){
-		var self = this;
+		var self = this, inputData = "";
 		
-		this.inputText.on('keyup', function(){
+		this.inputText.on('keyup paste', function(){
 			if (!this.value) {
+				inputData = "";
 				self.element.css('overflow','hidden');
 			} else {
-
-				var inputData = this.value;
-
-				$.post(self.href, { search: inputData } ).then(function(fromResolve){
+				if (inputData != this.value) {
 				
-					var fromResolve = $.parseJSON(fromResolve);
+					inputData = this.value;
+	
+					$.post(self.href, { search: inputData } ).then(function(fromResolve){
 					
-					var data = "";
-					for(var i = 0; i < fromResolve.length; i++ ) {
-						data += "<li class='list-group-item'>" + fromResolve[i] + "</li>";	
-					}
-
-					self.list.html(data);
-					self.element.css('overflow','visible');
-					
-				}).catch(function(fromReject){
-					console.log(fromReject);
-				});
+						var fromResolve = $.parseJSON(fromResolve);
+						
+						var data = "";
+						for(var i = 0; i < fromResolve.length; i++ ) {
+							data += "<li class='list-group-item'>" + fromResolve[i] + "</li>";	
+						}
+	
+						self.list.html(data);
+						self.element.css('overflow','visible');
+						
+					}).catch(function(fromReject){
+						console.log(fromReject);
+					});
+				}
 			}
 		});
 		
